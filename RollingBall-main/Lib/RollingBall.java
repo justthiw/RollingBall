30dc10ffefc033323a3455dac5cedc80a28e7bb0
Kwanchai <kwanchai.o@ku.th>
2026-07-02 16:28:17 +0700

    Add files via upload

---
+++ RollingBall-main/Lib/RollingBall.java
@@ -0,0 +1,12 @@
+package Lib;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RollingBall extends JPanel {
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawOval(100,90,80,80);
+        g.fillArc(100,90,80,80,0,180);
+    }
+}
